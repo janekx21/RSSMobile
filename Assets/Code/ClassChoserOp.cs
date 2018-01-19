53e271ba2845fc5ea055a6af8011b0297ad385e1
Janek <janekx21@gmail.com>
2018-01-19 23:55:51 +0100

    global import

---
+++ Assets/Code/ClassChoserOp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClassChoserOp : MonoBehaviour {
+    public OperatorConst local;
+    public Text nameText;
+    public Text codeNameText;
+    public Text ageText;
+    public Image portrai;
+    public Image background;
+	// Use this for initialization
+	void Start () {
+        if (!local) {
+            Destroy(gameObject);
+            return;
+        }
+        nameText.text = local.realName;
+        codeNameText.text = local.codeName;
+        ageText.text = local.alter.ToString();
+        portrai.sprite = local.portrai;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(IngameUI.instance.selected == this) {
+            background.color = Color.red;
+        }
+        else {
+            background.color = Color.white;
+        }
+	}
+}
