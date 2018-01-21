f037799c05411899521741ff77f58f83f9df1a1a
Janek <janekx21@gmail.com>
2018-01-21 16:19:03 +0100

    brakble

---
+++ Assets/Code/Breakable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+    public float maxHp = 3f;
+    float hp = 3f;
+	// Use this for initialization
+	void Start () {
+        hp = maxHp;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Break() {
+        Destroy(gameObject);
+    }
+
+    public void getHitLocal(float damage) {
+        hp -= damage;
+        if(hp <= 0) {
+            Break();
+        }
+    }
+}
